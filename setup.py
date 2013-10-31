6b1f380bb6ccaa6a528848bfdf7c8f8526c4ac4c
Chris Mutel <cmutel@gmail.com>
2013-10-31 08:55:42 +0100

    0.8.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bw2ui',
-    version="0.8",
+    version="0.8.1",
     packages=["bw2ui", "bw2ui.web"],
     package_data={'bw2ui.web': [
         "static/blueprint/*.css",
